ccc7873a3fbd670c1660ff9996f6dc24a6d77597
wforguo <wforguo@qq.com>
2021-12-02 22:14:00 +0800

    feat: 添加babel-polyfill到webpack配置当中

--- webpack-typesctipt/config/webpack.prod.js
+++ webpack-typesctipt/config/webpack.prod.js
@@ -5,14 +5,18 @@
  */
 const path = require("path");
 
+// 在每次 build 后移除你的dist目录（可配置），默认情况下它会读取 webpack 配置的output.path。
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // installed via npm
+
 module.exports = {
     mode: 'production',
     entry: {
-        app: path.resolve(__dirname, '../src/app.js')
+        app: ['babel-polyfill', path.resolve(__dirname, '../src/app.js')],
+        app2: ['babel-polyfill', path.resolve(__dirname, '../src/app2.js')],
     },
     output: {
         // path: path.resolve(__dirname, '../dist'),
-        filename: "js/[name].[hash:8].js",
+        filename: "js/[name].[chunkhash:8].js",
         // publicPath: '/',
     },
     module: {
@@ -31,5 +35,8 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(), // outputPath
+    ]
 }
