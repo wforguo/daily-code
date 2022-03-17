b4dafba4c8c3d8116189abcad3a4d1d06515156a
wforguo <wforguo@qq.com>
2022-03-17 17:41:59 +0800

    feat: cors学习

--- cors/server/app/routes/api/user.js
+++ cors/server/app/routes/api/user.js
@@ -21,13 +21,15 @@ router
             params = ctx.request.body;
         }
 
-        ctx.cookies.set("user", "cookie", {
-            // domain: 'localhost',  // 写cookie所在的域名
+        ctx.cookies.set("user", params.user, {
+            // domain: '127.0.0.1',  // 写cookie所在的域名
             // path: '/',       // 写cookie所在的路径
-            maxAge: 60 * 60 * 1000, // cookie有效时长 60分钟
-            expires: new Date('2023-03-15'),  // cookie失效时间
+            maxAge: 60 * 60 * 1000 * 24 * 7, // cookie有效时长 7天
+            expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),  // cookie失效时间
             httpOnly: false,  // 是否只用于http请求中获取（设置为true的话，客户端在控制台就获取不到）
-            // overwrite: false  // 是否允许重写
+            overwrite: false,  // 是否允许重写
+            secure: true,
+            sameSite: 'none',
         });
 
         ctx.body = {
@@ -42,39 +44,39 @@ router
 /**
  * 设置cookie
  */
-router
-    .post('/login',async ctx => {
-        let method = ctx.request.method || 'GET';
-        let params = {};
-        if (method === 'GET') {
-            params = ctx.request.query;
-        }
-        if (method === 'POST') {
-            params = ctx.request.body;
-        }
+.post('/login',async ctx => {
+    let method = ctx.request.method || 'GET';
+    let params = {};
+    if (method === 'GET') {
+        params = ctx.request.query;
+    }
+    if (method === 'POST') {
+        params = ctx.request.body;
+    }
 
-        ctx.cookies.set("user", "forguo", {
-            // domain: '127.0.0.1',  // 写cookie所在的域名
-            // path: '/',       // 写cookie所在的路径
-            maxAge: 60 * 60 * 1000, // cookie有效时长 60分钟
-            expires: new Date('2023-03-15'),  // cookie失效时间
-            httpOnly: false,  // 是否只用于http请求中获取（设置为true的话，客户端在控制台就获取不到）
-            overwrite: false  // 是否允许重写
-        });
+    ctx.cookies.set("user", params.user, {
+        // domain: '127.0.0.1',  // 写cookie所在的域名
+        // path: '/',       // 写cookie所在的路径
+        maxAge: 60 * 60 * 1000 * 24 * 7, // cookie有效时长 7天
+        expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),  // cookie失效时间
+        httpOnly: false,  // 是否只用于http请求中获取（设置为true的话，客户端在控制台就获取不到）
+        overwrite: false,  // 是否允许重写
+        secure: true,
+        sameSite: 'none',
+    });
 
-        ctx.body = {
-            data: {
-                ...params,
-            },
-            code: 200,
-            serverTime: Date.now(),
-        };
-    })
+    ctx.body = {
+        data: {
+            ...params,
+        },
+        code: 200,
+        serverTime: Date.now(),
+    };
+})
 
 /**
  * 接收cookie
  */
-router
     .post('/info',async ctx => {
         let user = {};
         if (ctx.headers.cookie) {
