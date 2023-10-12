ecda3c9578d64f818a12744fa807277c63990975
wforguo <wforguo@qq.com>
2023-10-12 22:38:23 +0800

    feat: 删除冗余代码

--- vue-app/vue3-app/src/views/TypeScript/index.vue
+++ vue-app/vue3-app/src/views/TypeScript/index.vue
@@ -9,21 +9,16 @@
 </template>
 
 <script lang="ts" setup>
-import { onBeforeMount } from 'vue'
 import rawCode from './index'
 
 onBeforeMount(() => {
     rawCode()
 })
-</script>
 
-<script lang="ts">
-// 使用了setup写法时，额外的name、title等信息，需要单独一个script标签，并且lang同setup的script标签
-export default {
+defineOptions({
     name: 'TsView',
     title: 'Ts',
     inheritAttrs: false,
     customOptions: {}
-}
+})
 </script>
-<style scoped></style>
