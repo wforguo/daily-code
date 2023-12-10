3bafc2986180e692937bbcb8b10059fc0434a543
wforguo <wforguo@users.noreply.github.com>
2023-12-10 22:25:26 +0800

    deploy: 66c3373a96c14082a27f6a0d57b108f7e0c9a58a

---
+++ vue3-app/js/index-08e27f5e.js
@@ -0,0 +1,2 @@
+import{_ as a}from"./index-248b4092.js";import{v as e,o as r,c,l as t,x as o,b as l,z as _}from"./vendor-43535c55.js";const i={},d={slot:"extra"};function m(f,p){const n=e("el-link"),s=e("el-result");return r(),c("div",null,[t(s,{icon:"info",title:"404",subTitle:"Page Not Found"},{default:o(()=>[l("template",d,[t(n,{type:"primary",size:"medium",href:"/"},{default:o(()=>[_("返回首页")]),_:1})])]),_:1})])}const h=a(i,[["render",m]]);export{h as default};
+//# sourceMappingURL=index-08e27f5e.js.map
